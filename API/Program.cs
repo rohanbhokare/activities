6999ffc996c1d4a4f626cdf2b0d809be397d0f4f
Rohan Bhokare <rohanbhokare92@gmail.com>
2023-06-17 13:02:38 +0530

    added react app

--- API/Program.cs
+++ API/Program.cs
@@ -20,7 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();  // this will register attribute routes
