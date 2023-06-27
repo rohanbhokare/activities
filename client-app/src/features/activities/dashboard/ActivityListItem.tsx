549f956a4e4290e9643f7854f3a79ca107964472
Rohan Bhokare <rohanbhokare92@gmail.com>
2023-06-27 21:14:59 +0530

    formik is added

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,6 +1,7 @@
 import { Activity } from '../../../app/models/activity';
 import { Button, Icon, Item, Segment } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
 
 
 interface Props {
@@ -26,7 +27,7 @@ export default function ActivityListItem({activity}: Props ) {
             </Segment>
             <Segment>
                 <span>
-                    <Icon name='clock' /> {activity.date}
+                    <Icon name='clock' /> {format(activity.date!,'dd MMM yyyy h:mm aa')}
                     <Icon name='marker' /> {activity.venue}
                 </span>
             </Segment>
